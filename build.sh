3f428095ac82ba5713defd2af4b6a93606bc5928
ning <ning.huang.ridson@gmail.com>
2024-11-24 16:01:43 +0800

    update build sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 docker build -f backend/base.dockerfile -t fba_base_server .
 docker build -f backend/backend.dockerfile -t fba_server .
+
+docker build -t fba_server -f backend/fba_server.dockerfile .
