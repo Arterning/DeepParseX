5ec2e78472a55d991f678bc315af5054da2d3a03
ning <ning.huang.ridson@gmail.com>
2025-01-01 16:41:11 +0800

    fix requirement

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,4 @@
 nohup pdm run fastapi dev main.py --host 0.0.0.0 >> fba.log 2>&1 & 
 # pdm run fastapi dev main.py --host 0.0.0.0 --port 8001 --reload
 # pdm run uvicorn main:app --host 0.0.0.0 --port 8001 --reload
-nohup fastapi dev  /home/kemove/liboyu_code/backend/main.py --host 0.0.0.0 --port 8001 >> fba.log 2>&1  
+nohup fastapi dev  /home/kemove/liboyu_code/backend/main.py --host 0.0.0.0 --port 8000 >> fba.log 2>&1  
