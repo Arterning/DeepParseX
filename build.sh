d16bd8ff4008b8937e59e9ebb55febee6aa45440
ning <ning.huang.ridson@gmail.com>
2025-01-16 11:06:41 +0800

    update docker build name

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # docker build -f backend/base.dockerfile -t fba_base_server .
 # docker build -f backend/backend.dockerfile -t fba_server .
 
-docker build -t fba_server -f backend/fba_server.dockerfile .
+docker build -t lda/fba_server -f backend/fba_server.dockerfile .
 docker image prune
