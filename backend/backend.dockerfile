f82f9062fab842a367ab3c4ec8e6b45505836ad4
ning <ning.huang.ridson@gmail.com>
2025-04-17 13:42:54 +0800

    update docker compose  file

--- backend/backend.dockerfile
+++
@@ -1,9 +0,0 @@
-FROM fba_base_server
-
-WORKDIR /fba
-
-COPY . .
-
-RUN mv backend/.env.docker /fba/backend/.env
-
-CMD ["uvicorn", "backend.main:app", "--host", "127.0.0.1", "--port", "8000"]
--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 # docker build -f backend/base.dockerfile -t fba_base_server .
 # docker build -f backend/backend.dockerfile -t fba_server .
+# docker build -f backend/fba_db.dockerfile -t fba_db .
 
-docker build -t lda/fba_server -f backend/fba_server.dockerfile .
+docker build -t fba_celery -f backend/celery.dockerfile .
+docker build -t fba_server -f backend/fba_server.dockerfile .
 docker image prune
