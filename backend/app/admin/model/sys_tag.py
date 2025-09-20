f870b9d69ba2abdc5a6b581f2694c112cdeeba3f
Arterning <ning.huang.ridson@gmail.com>
2025-09-20 15:53:45 +0800

    clean code

--- backend/app/admin/model/sys_tag.py
+++ backend/app/admin/model/sys_tag.py
@@ -10,9 +10,6 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from backend.common.model import Base, id_key, UserMixin
 from backend.app.admin.model.sys_tag_doc import sys_tag_doc
-from backend.app.admin.model.sys_tag_person import sys_tag_person
-from backend.app.admin.model.sys_tag_org import sys_tag_org
-from backend.app.admin.model.sys_tag_subject import sys_tag_subject
 
 class Tag(Base, UserMixin):
     """标签"""
@@ -23,17 +20,6 @@ class Tag(Base, UserMixin):
     
     name: Mapped[str] = mapped_column(sa.String(), default='', comment='标签名称')
 
-    subjects: Mapped[list['Subject']] = relationship(
-        init=False, secondary=sys_tag_subject
-    )
-
-    persons: Mapped[list['Person']] = relationship(
-        init=False, secondary=sys_tag_person
-    )
-
-    orgs: Mapped[list['SysOrg']] = relationship(
-        init=False, secondary=sys_tag_org
-    )
 
     docs: Mapped[list['SysDoc']] = relationship(
         init=False, secondary=sys_tag_doc
