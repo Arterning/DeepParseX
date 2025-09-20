d73a7ba798883f0a4c9071ba47d838fd9864874b
Arterning <ning.huang.ridson@gmail.com>
2025-09-20 15:27:43 +0800

    clean code

--- backend/app/admin/api/v1/sys/__init__.py
+++ backend/app/admin/api/v1/sys/__init__.py
@@ -19,17 +19,10 @@ from backend.app.admin.api.v1.sys.chat import router as chat_router
 from backend.app.admin.api.v1.sys.entity import router as entity_router
 from backend.app.admin.api.v1.sys.org import router as org_router
 from backend.app.admin.api.v1.sys.person import router as person_router
-from backend.app.admin.api.v1.sys.assets import router as assets_router
-from backend.app.admin.api.v1.sys.event import router as event_router
 from backend.app.admin.api.v1.sys.tag import router as tag_router
 from backend.app.admin.api.v1.sys.star_collect import router as star_collect_router
 from backend.app.admin.api.v1.sys.mail_box import router as mail_box_router
 from backend.app.admin.api.v1.sys.mail_msg import router as mail_msg_router
-from backend.app.admin.api.v1.sys.subject import router as subject_router
-from backend.app.admin.api.v1.sys.scandal import router as scandal_router
-from backend.app.admin.api.v1.sys.news import router as news_router
-from backend.app.admin.api.v1.sys.social_account import router as social_account_router
-from backend.app.admin.api.v1.sys.social_account_post import router as social_account_post_router
 from backend.app.admin.api.v1.sys.subject_predict_object import router as subject_predict_object_router
 from backend.app.admin.api.v1.sys.upload_task import router as upload_task_router
 
@@ -53,16 +46,9 @@ router.include_router(chat_router, prefix='/chat', tags=['对话'])
 router.include_router(entity_router,prefix='/entity',tags=['实体'])
 router.include_router(org_router,prefix='/org',tags=['组织'])
 router.include_router(person_router,prefix='/person',tags=['人物'])
-router.include_router(assets_router,prefix='/asset',tags=['资产'])
-router.include_router(event_router,prefix='/events',tags=['事件'])
 router.include_router(tag_router,prefix='/tags',tags=['标签'])
 router.include_router(star_collect_router, prefix='/star_collect', tags=['收藏'])
 router.include_router(mail_box_router,prefix='/mailbox',tags=['邮箱'])
 router.include_router(mail_msg_router,prefix='/mailmsg',tags=['邮件'])
-router.include_router(subject_router,prefix='/subject',tags=['议题'])
-router.include_router(scandal_router, prefix='/scandal', tags=['黑料库'])
-router.include_router(news_router,prefix='/news',tags=['新闻'])
-router.include_router(social_account_router,prefix='/social_account',tags=['社交账号'])
-router.include_router(social_account_post_router,prefix='/social_post',tags=['社交帖子'])
 router.include_router(subject_predict_object_router,prefix='/spo',tags=['要素提取'])
 router.include_router(upload_task_router,prefix='/upload_task',tags=['任务管理'])
--- backend/app/admin/schema/account_pwd.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-from backend.common.schema import SchemaBase
-
-
-class AssetsParam(SchemaBase):
-    doc_ids:list
