8a19b9573cc6d24ed39321d52663d3162eb3558f
ning.huang <ning.huang.ridson@gmail.com>
2025-12-06 22:15:35 +0800

    fix

--- backend/app/admin/api/v1/sys/doc.py
+++ backend/app/admin/api/v1/sys/doc.py
@@ -178,9 +178,11 @@ async def preview_pdf(obj_name: str):
 )
 async def search(
     tokens: Annotated[str | None, Query()] = None,
+    query: Annotated[str | None, Query()] = None,
     page: Annotated[int | None, Query()] = None,
     size: Annotated[int | None, Query()] = None
 ) -> ResponseModel:
+    tokens = tokens or query
     docs = await sys_doc_service.search(tokens=tokens, page=page, size=size)
     return response_base.success(data=docs)
 
