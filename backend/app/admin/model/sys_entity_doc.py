a0d369c8aad1a0caeff9142fe474f35f1f8c75fd
ning.huang <ning.huang.ridson@gmail.com>
2026-01-20 14:33:16 +0800

    fix:：PostgreSQL 的序列（自增计数器）与已有数据不同步，导致生成的 id 与已存在的记录冲突。

--- backend/app/admin/model/sys_entity_doc.py
+++ backend/app/admin/model/sys_entity_doc.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from sqlalchemy import BIGINT, Column, ForeignKey, Integer, Table
+from sqlalchemy import BIGINT, Column, ForeignKey, Integer, Table, UniqueConstraint
 
 from backend.common.model import MappedBase
 
 sys_entity_doc = Table(
     'sys_entity_doc',
     MappedBase.metadata,
-    Column('id', BIGINT, primary_key=True, unique=True, index=True, autoincrement=True, comment='主键ID'),
     Column('entity_id', BIGINT, ForeignKey('sys_entity.id', ondelete='CASCADE'), primary_key=True, comment='实体ID'),
     Column('doc_id', BIGINT, ForeignKey('sys_doc.id', ondelete='CASCADE'), primary_key=True, comment='文档ID'),
 )
